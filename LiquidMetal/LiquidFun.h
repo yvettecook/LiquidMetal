3f1612df939b31adc70cb0deccc96d24b60e72b9
Yvette <yvettecook@gmail.com>
2015-11-07 18:22:43 +0000

    Part 1 of tutorial finished

--- LiquidMetal/LiquidFun.h
+++ LiquidMetal/LiquidFun.h
@@ -16,10 +16,19 @@ typedef struct Vector2D {
     float y;
 } Vector2D;
 
+typedef struct Size2D {
+    float width;
+    float height;
+} Size2D;
+
 #endif
 
 @interface LiquidFun : NSObject
 
 + (void)createWorldWithGravity:(Vector2D)gravity;
++ (void *)createParticleSystemWithRadius:(float)radius dampingStrength:(float)dampingStrength gravityScale:(float)gravityScale density:(float)density;
++ (void)createParticleBoxForSystem:(void *)particleSystem position:(Vector2D)position size:(Size2D)size;
++ (int)particleCountForSystem:(void *)particleSystem;
++ (void *)particlePositionsForSystem:(void *)particleSystem;
 
 @end
--- LiquidMetal/ViewController.swift
+++ LiquidMetal/ViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let gravity: Float = 9.80665
+    let ptmRatio: Float = 320
+    let particleRadius: Float = 9
+    var particleSystem: UnsafeMutablePointer<Void>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LiquidFun.createWorldWithGravity(Vector2D(x: 0, y: -gravity))
+        
+        particleSystem = LiquidFun.createParticleSystemWithRadius(particleRadius / ptmRatio, dampingStrength: 0.2, gravityScale: 1, density: 1.2)
+        
+        let screenSize: CGSize = UIScreen.mainScreen().bounds.size
+        let screenWidth = Float(screenSize.width)
+        let screenHeight = Float(screenSize.height)
+        
+        LiquidFun.createParticleBoxForSystem(particleSystem, position: Vector2D(x: screenWidth * 0.5 / ptmRatio, y: screenHeight * 0.5 / ptmRatio), size: Size2D(width: 50 / ptmRatio, height: 50 / ptmRatio))
+        
+        printParticleInfo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+   
+    
+    func printParticleInfo() {
+        let count = Int(LiquidFun.particleCountForSystem(particleSystem))
+        print("There are \(count) particles present")
+        
+        let positions = UnsafePointer<Vector2D>(LiquidFun.particlePositionsForSystem(particleSystem))
+        
+        for i in 0..<count {
+            let position = positions[i]
+            print("particle: \(i) position: \(position.x), \(position.y)")
+        }
     }
 
 
